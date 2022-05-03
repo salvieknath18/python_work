d1add43f75894137476f7ef32064e3e5f65d2b5b
salvieknath18 <68014082+salvieknath18@users.noreply.github.com>
2022-05-03 15:53:42 +0530

    Update settings.py

--- Django/Student_Management/Student_Management/settings.py
+++ Django/Student_Management/Student_Management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't&68p8rqo0ah@l7n974_717hxj1kg6jehzwre64cq%s-9@3y*d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
